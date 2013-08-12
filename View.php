464dedc78bb6eb0d7c02996d566fca573f9c03d2
Pavel <xPaw@users.noreply.github.com>
2013-08-12 20:07:54 +0300

    Use bootstrap 3

--- View.php
+++ View.php
@@ -2,13 +2,14 @@
 	require __DIR__ . '/SourceQuery/SourceQuery.class.php';
 	
 	// Edit this ->
-	define( 'SQ_SERVER_ADDR', 'gs2.my-run.de' );
+	define( 'SQ_SERVER_ADDR', 'localhost' );
 	define( 'SQ_SERVER_PORT', 27015 );
 	define( 'SQ_TIMEOUT',     1 );
 	define( 'SQ_ENGINE',      SourceQuery :: SOURCE );
 	// Edit this <-
 	
 	$Timer = MicroTime( true );
+	
 	$Query = new SourceQuery( );
 	
 	$Info    = Array( );
@@ -29,6 +30,8 @@
 	}
 	
 	$Query->Disconnect( );
+	
+	$Timer = Number_Format( MicroTime( true ) - $Timer, 4, '.', '' );
 ?>
 <!DOCTYPE html>
 <html>
@@ -36,48 +39,48 @@
 	<meta charset="utf-8">
 	<title>Source Query PHP Class</title>
 	
-	<link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css">
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
 	<style type="text/css">
-		footer {
-			padding: 30px 0;
-			margin-top: 60px;
-			border-top: 1px solid #E5E5E5;
-			background-color: whiteSmoke;
+		.jumbotron {
+			margin-top: 30px;
+			border-radius: 0;
 		}
-		footer p {
-			margin-bottom: 0;
-			color: #777;
-		}
-		.page-header h1 {
-			font-size: 60px;
-			font-weight: 200;
-			line-height: 1;
-			letter-spacing: -1px;
+		
+		.table thead th {
+			background-color: #428BCA;
+			border-color: #428BCA !important;
+			color: #FFF;
 		}
 	</style>
 </head>
 
 <body>
     <div class="container">
-    	<div class="page-header">
+		<div class="jumbotron">
 			<h1>Source Query PHP Class</h1>
-		</div>
-
-<?php if( isset( $Exception ) ): ?>
-		<div class="alert alert-error">
-			<h4 class="alert-heading"><?php echo Get_Class( $Exception ); ?> at line <?php echo $Exception->getLine( ); ?></h4>
-			<?php echo htmlspecialchars( $Exception->getMessage( ) ); ?>
+			
+			<p>This class was created to query game server which use the Source query protocol, this includes all source games, half-life 1 engine games and Call of Duty: Modern Warfare 3</p>
+			
+			<p>
+				<a class="btn btn-large btn-primary" href="http://xpaw.ru">Made by xPaw</a>
+				<a class="btn btn-large btn-primary" href="https://github.com/xPaw/PHP-Source-Query-Class">View on GitHub</a>
+				<a class="btn btn-large btn-danger" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA 3.0</a>
+			</p>
 		</div>
 		
-		<h3>Stack trace</h3>
-		<pre><?php echo $e->getTraceAsString(); ?></pre>
+<?php if( isset( $Exception ) ): ?>
+		<div class="panel panel-primary">
+			<div class="panel-heading"><?php echo Get_Class( $Exception ); ?> at line <?php echo $Exception->getLine( ); ?></div>
+			<p><b><?php echo htmlspecialchars( $Exception->getMessage( ) ); ?></b></p>
+			<p><?php echo nl2br( $e->getTraceAsString(), false ); ?></p>
+		</div>
 <?php else: ?>
 		<div class="row">
-			<div class="span6">
+			<div class="col-sm-6">
 				<table class="table table-bordered table-striped">
 					<thead>
 						<tr>
-							<th colspan="2">Server Info</th>
+							<th colspan="2">Server Info <em>(queried in <?php echo $Timer; ?>s)</em></th>
 						</tr>
 					</thead>
 					<tbody>
@@ -99,11 +102,15 @@
 ?></td>
 						</tr>
 <?php endforeach; ?>
+<?php else: ?>
+						<tr>
+							<td colspan="2">No information received</td>
+						</tr>
 <?php endif; ?>
 					</tbody>
 				</table>
 			</div>
-			<div class="span6">
+			<div class="col-sm-6">
 				<table class="table table-bordered table-striped">
 					<thead>
 						<tr>
@@ -119,7 +126,7 @@
 <?php endforeach; ?>
 <?php else: ?>
 						<tr>
-							<td>No players in da house!</td>
+							<td>No players in da house</td>
 						</tr>
 <?php endif; ?>
 					</tbody>
@@ -127,8 +134,8 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="span12">
-				<table class="table table-condensed table-bordered table-striped">
+			<div class="col-sm-12">
+				<table class="table table-bordered table-striped">
 					<thead>
 						<tr>
 							<th colspan="2">Rules</th>
@@ -149,14 +156,5 @@
 		</div>
 <?php endif; ?>
 	</div>
-	
-	<footer>
-		<div class="container">
-			<p class="pull-right">Generated in <span class="badge badge-success"><?php echo Number_Format( ( MicroTime( true ) - $Timer ), 4, '.', '' ); ?>s</span></p>
-			
-			<p>Made by <a href="http://xpaw.ru" target="_blank">xPaw</a>, source code available on <a href="https://github.com/xPaw/PHP-Source-Query-Class" target="_blank">GitHub</a></p>
-			<p>Code licensed under the <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">CC BY-NC-SA 3.0</a></p>
-		</div>
-	</footer>
 </body>
 </html>
